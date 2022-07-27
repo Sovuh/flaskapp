41de6e41e882b84ab07a7a608e54a0aac2ac6bd5
Sovuh <all-light1@mail.ru>
2022-07-27 13:05:20 +0300

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,8 +1,7 @@
 from flask import Flask
-
 app = Flask(__name__)
-
-
-@app.route('/')
-def hello():
-    return 'Hello, World!'
+@app.route('/') 
+def hello_world():
+   return 'Hello from Flask!' 
+if __name__ == '__main__':
+   app.run()
